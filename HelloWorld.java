07c4a5e29aaf0b2c410e6c5ea43a60cf8f68fbda
vimarshpandey <vimarsh240501@gmail.com>
2023-10-15 14:25:31 +0530

    first programs in java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    int a = 9;
+    float b = 0.99f;
+    char c = '@';
+    String d = "The price of one pizza is ";
+    System.out.println(d+"$"+a+" and $"+b+" is GST "+c+" Pizza Hut");
+  }
+}
---
+++ UserInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UserInput
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name: ");
+        String name = scan.nextLine();
+
+        System.out.println("What is your age: ");
+        int age = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("What is you favourite game: ");
+        String game = scan.nextLine();
+
+        System.out.println("Your name is "+name+" and your age is "+age+" years. You like to play "+game);
+    }
+}
